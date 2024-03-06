51f1fd27ae630126f5e9cb4398805c2f0d8ab72f
oliverfelix <jayandhans484@gmail.com>
2024-03-07 00:30:12 +0530

    hyperparameter training performed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,10 @@ from datetime import datetime
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
+
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
@@ -59,7 +63,10 @@ if __name__=="__main__":
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr = data_transformation.initiate_data_transformation(train_data,test_data)
     
+    
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
 
 
